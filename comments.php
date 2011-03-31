3518d155bbbf9ff082eb8e7616e23bdf7e1b9c6d
David Trejo <david.daniel.trejo@gmail.com>
2011-03-31 15:44:10 -0400

    tabs2spaces: soft tabs 2 spaces

--- comments.php
+++ comments.php
@@ -1,71 +1,71 @@
 <?php // Do not delete these lines
-	if (!empty($_SERVER['SCRIPT_FILENAME']) && 'comments.php' == basename($_SERVER['SCRIPT_FILENAME']))
-		die ('Please do not load this page directly. Thanks!');
-	if (!empty($post->post_password)) { // if there's a password
-		if ($_COOKIE['wp-postpass_' . COOKIEHASH] != $post->post_password) { ?>
-			<p class="nocomments">This post is password protected. Enter the password to view comments.</p>
-			<?php return;
-		}
-	}
+  if (!empty($_SERVER['SCRIPT_FILENAME']) && 'comments.php' == basename($_SERVER['SCRIPT_FILENAME']))
+    die ('Please do not load this page directly. Thanks!');
+  if (!empty($post->post_password)) { // if there's a password
+    if ($_COOKIE['wp-postpass_' . COOKIEHASH] != $post->post_password) { ?>
+      <p class="nocomments">This post is password protected. Enter the password to view comments.</p>
+      <?php return;
+    }
+  }
 ?>
 
 <?php if ($comments) : ?>
-	<h3><?php comments_number('No Comments', 'One Comment', '% Comments' );?></h3>
+  <h3><?php comments_number('No Comments', 'One Comment', '% Comments' );?></h3>
 
-	<div class="comment-list">
-		<?php foreach ($comments as $comment) : ?>
-			<div id="comment-<?php comment_ID() ?>">
-				<div class="author">
-					<?php echo get_avatar( $comment, 64 ); ?>
-					<h3><?php comment_author_link() ?></h3>
-					<div class="comment-date"><?php comment_date('F jS, Y') ?></div>
-				</div>
-				<div class="comment">
-					<?php edit_comment_link('edit','',''); ?>
-					<?php if ($comment->comment_approved == '0') : echo "<p><em>Your comment is awaiting moderation.</em></p>"; endif; ?>
-					<?php comment_text() ?>
-				</div>
-			</div>
-		<?php endforeach; ?>
-	</div>
+  <div class="comment-list">
+    <?php foreach ($comments as $comment) : ?>
+      <div id="comment-<?php comment_ID() ?>">
+        <div class="author">
+          <?php echo get_avatar( $comment, 64 ); ?>
+          <h3><?php comment_author_link() ?></h3>
+          <div class="comment-date"><?php comment_date('F jS, Y') ?></div>
+        </div>
+        <div class="comment">
+          <?php edit_comment_link('edit','',''); ?>
+          <?php if ($comment->comment_approved == '0') : echo "<p><em>Your comment is awaiting moderation.</em></p>"; endif; ?>
+          <?php comment_text() ?>
+        </div>
+      </div>
+    <?php endforeach; ?>
+  </div>
 <?php else : // this is displayed if there are no comments so far ?>
-	<?php if ('open' == $post->comment_status) : ?>
-	<?php else : ?>
-		<p class="nocomments">Comments are closed.</p>
-	<?php endif; ?>
+  <?php if ('open' == $post->comment_status) : ?>
+  <?php else : ?>
+    <p class="nocomments">Comments are closed.</p>
+  <?php endif; ?>
 <?php endif; ?>
 
 <?php if ('open' == $post->comment_status) : ?>
-	<div class="new-comment">
-		<h3>Leave a Reply</h3>
+  <div class="new-comment">
+    <h3>Leave a Reply</h3>
 
-		<?php if ( get_option('comment_registration') && !$user_ID ) : ?>
-			<p>You must be <a href="<?php echo get_option('siteurl'); ?>/wp-login.php?redirect_to=<?php echo urlencode(get_permalink()); ?>">logged in</a> to post a comment.</p>
-		<?php else : ?>
-			<form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post" id="commentform">
-				<?php if ( $user_ID ) : ?>
-					<p>
-						Logged in as
-						<a href="<?php echo get_option('siteurl'); ?>/wp-admin/profile.php"><?php echo $user_identity; ?></a>.
-						<a href="<?php echo get_option('siteurl'); ?>/wp-login.php?action=logout" title="Log out of this account">Log out &raquo;</a>
-					</p>
-				<?php else : ?>
-					<table><tr>
-						<td><label for="author">Name <?php if ($req) echo "(required)"; ?></label></td>
-						<td><input type="text" name="author" id="author" value="<?php echo $comment_author; ?>" size="22" tabindex="1" /></td>
-					</tr><tr>
-						<td><label for="email">Email (will not be shared<?php if ($req) echo ", required"; ?>)</label></td>
-						<td><input type="text" name="email" id="email" value="<?php echo $comment_author_email; ?>" size="22" tabindex="2" /></td>
-					</tr><tr>
-						<td><label for="url">Website</label></td>
-						<td><input type="text" name="url" id="url" value="<?php echo $comment_author_url; ?>" size="22" tabindex="3" /></td>
-					</tr></table>
-				<?php endif; ?>
-				<p><textarea name="comment" id="comment" cols="100%" rows="10" tabindex="4"></textarea></p>
-				<p class="clearfix"><input name="submit" type="submit" id="submit" tabindex="5" value="Submit Comment" /></p>
-				<input type="hidden" name="comment_post_ID" value="<?php echo $id; ?>" />
-				<?php do_action('comment_form', $post->ID); ?>
-			</form>
-		<?php endif; ?>
-	</div>
+    <?php if ( get_option('comment_registration') && !$user_ID ) : ?>
+      <p>You must be <a href="<?php echo get_option('siteurl'); ?>/wp-login.php?redirect_to=<?php echo urlencode(get_permalink()); ?>">logged in</a> to post a comment.</p>
+    <?php else : ?>
+      <form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post" id="commentform">
+        <?php if ( $user_ID ) : ?>
+          <p>
+            Logged in as
+            <a href="<?php echo get_option('siteurl'); ?>/wp-admin/profile.php"><?php echo $user_identity; ?></a>.
+            <a href="<?php echo get_option('siteurl'); ?>/wp-login.php?action=logout" title="Log out of this account">Log out &raquo;</a>
+          </p>
+        <?php else : ?>
+          <table><tr>
+            <td><label for="author">Name <?php if ($req) echo "(required)"; ?></label></td>
+            <td><input type="text" name="author" id="author" value="<?php echo $comment_author; ?>" size="22" tabindex="1" /></td>
+          </tr><tr>
+            <td><label for="email">Email (will not be shared<?php if ($req) echo ", required"; ?>)</label></td>
+            <td><input type="text" name="email" id="email" value="<?php echo $comment_author_email; ?>" size="22" tabindex="2" /></td>
+          </tr><tr>
+            <td><label for="url">Website</label></td>
+            <td><input type="text" name="url" id="url" value="<?php echo $comment_author_url; ?>" size="22" tabindex="3" /></td>
+          </tr></table>
+        <?php endif; ?>
+        <p><textarea name="comment" id="comment" cols="100%" rows="10" tabindex="4"></textarea></p>
+        <p class="clearfix"><input name="submit" type="submit" id="submit" tabindex="5" value="Submit Comment" /></p>
+        <input type="hidden" name="comment_post_ID" value="<?php echo $id; ?>" />
+        <?php do_action('comment_form', $post->ID); ?>
+      </form>
+    <?php endif; ?>
+  </div>
 <?php endif; ?>
--- index.php
+++ index.php
@@ -3,36 +3,36 @@
   <!-- Here we go with the hacky stuff -->
   <!-- Not sure how to best do this so it doesn't conflict with other pages -->
   <!-- Attach orbit CSS+JS -->
-	<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/orbit-1.2.3.css">
-	<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/jquery-1.5.1.min.js"></script>
-	<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/jquery.orbit-1.2.3.min.js"></script>
-	<!-- [if IE]>
-	     <style type="text/css">
-	      .timer { display: none !important; }
-	      div.caption { background:transparent; filter:progid:DXImageTransform.Microsoft.gradient(startColorstr=#99000000,endColorstr=#99000000);zoom: 1; }
-	    </style>
-	<![endif]-->
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/orbit-1.2.3.css">
+  <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/jquery-1.5.1.min.js"></script>
+  <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/jquery.orbit-1.2.3.min.js"></script>
+  <!-- [if IE]>
+       <style type="text/css">
+        .timer { display: none !important; }
+        div.caption { background:transparent; filter:progid:DXImageTransform.Microsoft.gradient(startColorstr=#99000000,endColorstr=#99000000);zoom: 1; }
+      </style>
+  <![endif]-->
 
-	<!-- Run orbit -->
-	<script type="text/javascript">
-		$(window).load(function() {
-			$('#featured').orbit();
-		});
-	</script>
+  <!-- Run orbit -->
+  <script type="text/javascript">
+    $(window).load(function() {
+      $('#featured').orbit();
+    });
+  </script>
 
-	<div class="showcase">
-		<div class="content">
-			<div id="featured">
-  			<div class="content" style="background: url(<?php bloginfo('template_url'); ?>/images/dummy-images/coffee.jpg);">
-  				<h2>Orbit does content now.</h2>
-  				<h3>Highlight me...I'm text.</h3>
-  			</div>
-  			<a href=""><img src="<?php bloginfo('template_url'); ?>/images/dummy-images/overflow.jpg" /></a>
-  			<img src="<?php bloginfo('template_url'); ?>/images/dummy-images/captions.jpg" data-caption="#htmlCaption" />
-  			<img src="<?php bloginfo('template_url'); ?>/images/dummy-images/features.jpg"  />
-  		</div>
-  		<!-- Captions for Orbit -->
-  		<span class="orbit-caption" id="htmlCaption"><strong>I'm A Badass Caption:</strong> I can haz <a href="#">links</a>, <em>style</em> or anything that is valid markup :)</span>
+  <div class="showcase">
+    <div class="content">
+      <div id="featured">
+        <div class="content" style="background: url(<?php bloginfo('template_url'); ?>/images/dummy-images/coffee.jpg);">
+          <h2>Orbit does content now.</h2>
+          <h3>Highlight me...I'm text.</h3>
+        </div>
+        <a href=""><img src="<?php bloginfo('template_url'); ?>/images/dummy-images/overflow.jpg" /></a>
+        <img src="<?php bloginfo('template_url'); ?>/images/dummy-images/captions.jpg" data-caption="#htmlCaption" />
+        <img src="<?php bloginfo('template_url'); ?>/images/dummy-images/features.jpg"  />
+      </div>
+      <!-- Captions for Orbit -->
+      <span class="orbit-caption" id="htmlCaption"><strong>I'm A Badass Caption:</strong> I can haz <a href="#">links</a>, <em>style</em> or anything that is valid markup :)</span>
 
       <!-- <img src="rapt.png">
       <h2>Robots Are People Too</h2>
@@ -40,24 +40,24 @@
       <p><a href="#">More info...</a></p>
       <div class="clear"></div> -->
 
-		</div> <!-- end orbit -->
-		<div class="shadow-bottom"></div>
-	</div>
+    </div> <!-- end orbit -->
+    <div class="shadow-bottom"></div>
+  </div>
 
-	<div class="main">
-		<div class="content">
-			<?php while (have_posts()) : the_post(); ?>
-				<div class="post" id="<?php the_ID(); ?>">
-					<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-					<div class="entry">
-						<?php the_content(); ?>
-					</div>
-				</div>
-			<?php endwhile; ?>
+  <div class="main">
+    <div class="content">
+      <?php while (have_posts()) : the_post(); ?>
+        <div class="post" id="<?php the_ID(); ?>">
+          <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+          <div class="entry">
+            <?php the_content(); ?>
+          </div>
+        </div>
+      <?php endwhile; ?>
 
-			<?php next_posts_link('&laquo; Older Entries') ?>
-			<?php previous_posts_link('Newer Entries &raquo;') ?>
-		</div>
-	</div>
+      <?php next_posts_link('&laquo; Older Entries') ?>
+      <?php previous_posts_link('Newer Entries &raquo;') ?>
+    </div>
+  </div>
 
 <?php get_footer(); ?>
