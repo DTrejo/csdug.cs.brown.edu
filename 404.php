7450568ac1879dfd82247d7a1513c7bf268085e5
Evan Wallace <evan@evan-wallaces-macbook-pro.local>
2011-03-31 16:20:01 -0400

    changed more pages to the correct template

--- 404.php
+++ 404.php
@@ -1,19 +1,13 @@
 <?php get_header(); ?>
 
-
-
-    <div class="post">
-
-      <h2>Page Not Found</h2>
-
-      <p>Looks like the page you're looking for isn't here anymore. Try browsing the <a href="">categories</a>, <a href="">archives</a>, or using the search box below.</p>
-
-      <?php include(TEMPLATEPATH . '/searchform.php'); ?>
-
-    </div> <!-- .post -->
-
-
-
-<?php get_sidebar(); ?>
+  <div class="main">
+    <div class="content">
+      <div class="post">
+        <h2>Page Not Found</h2>
+        <p>Looks like the page you're looking for isn't here anymore. Try browsing from the <a href="<?php esc_attr( bloginfo('url') ); ?>">home page</a> or using the search box below.</p>
+        <p><?php include(TEMPLATEPATH . '/searchform.php'); ?></p>
+      </div>
+    </div>
+  </div>
 
 <?php get_footer(); ?>
--- page.php
+++ page.php
@@ -1,23 +1,16 @@
 <?php get_header(); ?>
 
-
-
-    <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
-
-    <div class="post" id="<?php the_ID(); ?>">
-
-      <h2><?php the_title(); ?></h2>
-
-      <div class="entry">
-        <?php the_content(); ?>
-      </div>
-
-    </div> <!-- .post -->
-
-    <?php endwhile; endif; ?>
-
-
-
-<?php get_sidebar(); ?>
+  <div class="main">
+    <div class="content">
+      <?php while (have_posts()) : the_post(); ?>
+        <div class="post" id="<?php the_ID(); ?>">
+          <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+          <div class="entry">
+            <?php the_content(); ?>
+          </div>
+        </div>
+      <?php endwhile; ?>
+    </div>
+  </div>
 
 <?php get_footer(); ?>
